debd0ae2f3b23bd0f202246dc2a868a43184f6c2
Sakurafall <cldeng2024@163.com>
2025-06-22 18:51:34 +0800

    Update Grad_ptych.m

--- functions/Grad_ptych.m
+++ functions/Grad_ptych.m
@@ -1,40 +1,40 @@
-function [sample,MSE] = Grad_ptych(CCD,P,sample,epoch,deta,Z,pixSize,lambda,alpha)
-%CCD    衍射采集到的数据（多维矩阵）
-%P      探针矩阵
-%sample 样品初始复振幅
-%epoch  迭代次数
-%deta   移动步长
-%Z      衍射距离
+function [sample,MSE] = Grad_ptych(CCD,P,sample,epoch,deta,Z,pixSize,lambda,alpha,segd)
+%CCD    diffraction image dataset
+%P      light probe
+%sample initial sample amplitude
+%epoch  iteration
+%deta   move step length
+%Z      diffraction distance
 G_u=ones(size(sample));
 [yP,xP] = size(P);
 r=yP;c=xP;
 [M,N] = size(sample);
 for i = 1:epoch
     k = 1;mse=zeros(r,c);
-    for ii = -1:1
-        for jj = -1:1
+    for ii = segd
+        for jj = segd
             dy=deta*ii;dx=deta*jj;
            
             sub_u=sample(M/2-r/2+dy:M/2+r/2-1+dy,N/2-c/2+dx:N/2+c/2-1+dx);
             exitwave = sub_u.*P; 
-            U1 = Propagate(exitwave,Z,pixSize,lambda);%正向传播产生预测图
+            U1 = Propagate(exitwave,Z,pixSize,lambda);%forward
 
-            dU=(abs(U1)-abs(CCD(:,:,k))).*U1.*2;%计算CCD面误差梯度
+            dU=(abs(U1)-abs(CCD(:,:,k))).*U1.*2;%error gradient in surface of CCD
 
-            mse=mse+(abs(U1)-abs(CCD(:,:,k))).^2./(r*c);%均方误差
+            mse=mse+(abs(U1)-abs(CCD(:,:,k))).^2./(r*c);%mean squared error
         
-            G_subu=Propagate(dU,-Z,pixSize,lambda).*conj(P);%反向传播误差梯度得到样品面的梯度
+            G_subu=Propagate(dU,-Z,pixSize,lambda).*conj(P);%backpropagation error gradient to sample surface
         
             G_u(M/2-r/2+dy:M/2+r/2-1+dy,N/2-c/2+dx:N/2+c/2-1+dx)...
-            =G_u(M/2-r/2+dy:M/2+r/2-1+dy,N/2-c/2+dx:N/2+c/2-1+dx)-alpha*G_subu;%扩展到复数域的梯度下降
+            =G_u(M/2-r/2+dy:M/2+r/2-1+dy,N/2-c/2+dx:N/2+c/2-1+dx)-alpha*G_subu; %Gradient descent in complex domain
 
         k = k + 1;
            
         end
          sample=G_u;
     end
-    MSE(i)=sum(mse(:))/(r*c*9);
-    disp(['第',num2str(i),'次迭代误差：',num2str(MSE(i))])
+    MSE(i)=sum(mse(:))/(r*c*length(segd).^2);
+    disp(['iteration:',num2str(i),'   error：',num2str(MSE(i))])
 end
 
 end
